dda11f2daa66cdbe7774ed5245a60e2804744b27
unknown <raulalcoba23@gmail.com>
2025-03-28 12:09:00 -0300

    se arreglan errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router"
-import NatvarC from "./componentes/navbar/NavbarC"
-import IniciarSesion from "./paginas/IniciarSesion"
+import { BrowserRouter as Router, } from "react-router"
+import NavbarC from "./componentes/navbar/NavbarC"
+/* import IniciarSesion from "./paginas/IniciarSesion" */
 
 const App = () => {
   return (
     <>
-      <NatvarC />
-      <Routes>
-        <Route path="/iniciarsesion" element={<IniciarSesion />} />
-      </Routes>
+      <Router>
+        <NavbarC />
+
+      </Router>
+     
     </>
   );
 };
