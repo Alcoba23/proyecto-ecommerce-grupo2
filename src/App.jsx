c7a3726e30e549ed54c5067b6a1fb71c6fa5552d
valejusplay <vg9917755@gmail.com>
2025-03-29 13:15:23 -0300

    Revert "Feature pagina principal2"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Homepage from "./paginas/homepage";
-import IniciarSesion from "./paginas/IniciarSesion";
+import { BrowserRouter as Router, } from "react-router"
+import NavbarC from "./componentes/navbar/NavbarC"
+/* import IniciarSesion from "./paginas/IniciarSesion" */
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="i" element={<IniciarSesion />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <NavbarC />
+
+      </Router>
+     
+    </>
   );
 };
 
--- src/componentes/footer/FooterC.jsx
+++
@@ -1,9 +0,0 @@
-const FooterC = () => {
-  return (
-    <footer className="bg-body-tertiarya p-5 text-center">
-      <h3>Footer</h3>
-    </footer>
-  );
-};
-
-export default FooterC;
