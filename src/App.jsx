cefad0e1c376c92765d97f86d49bb2253eb87f39
unknown <raulalcoba23@gmail.com>
2025-03-30 21:39:26 -0300

    se borra algo

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Homepage from "./paginas/Homepage";
 import IniciarSesion from "./paginas/IniciarSesion";
 import NavbarC from "./componentes/navbar/NavbarC";
- feature-pagina-principal2
 import FooterC from "./componentes/footer/FooterC";
-
 import Registro from "./paginas/Registro";
  dev
 
 const App = () => {
   return (
-    <Router>
-      <NavbarC />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="i" element={<IniciarSesion />} />
-        <Route path="r" element={<Registro/>}/>
-      </Routes>
-      <FooterC />
-    </Router>
+    <>
+      <Router>
+        <NavbarC />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="i" element={<IniciarSesion />} />
+          <Route path="r" element={<Registro />} />
+        </Routes>
+        <FooterC />
+      </Router>
+    </>
   );
 };
 
